f80e7d867aeb519dd25ada58f982179e5dd9ad7d
Khadija Rauf <72842177+Khadija-Rauf@users.noreply.github.com>
2026-07-04 19:22:55 +0500

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -6,8 +6,6 @@ import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 import java.util.concurrent.Future;
 
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
         ExecutorService executor = Executors.newFixedThreadPool(FileChunker.Chunks);
